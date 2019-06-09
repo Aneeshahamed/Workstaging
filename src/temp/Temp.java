eb3aea0c77dc5ea2c7efb6acc74f0d373f7cf2fd
MyFirstGit <aneesh.fabulous@gmail.com>
2019-06-09 12:21:54 +0530

    Un commet updated

--- src/temp/Temp.java
+++ src/temp/Temp.java
@@ -13,29 +13,29 @@ import org.testng.annotations.Test;
 
 public class Temp 
 {
-//	String path = System.setProperty("webdriver.chrome.driver","E:\\Automation\\Selenium Driver\\chromedriver_win32\\chromedriver.exe");
-//	ChromeDriver driver = new ChromeDriver();
-//	WebDriverWait wait20 = new WebDriverWait(driver, 40);
+	String path = System.setProperty("webdriver.chrome.driver","E:\\Automation\\Selenium Driver\\chromedriver_win32\\chromedriver.exe");
+	ChromeDriver driver = new ChromeDriver();
+	WebDriverWait wait20 = new WebDriverWait(driver, 40);
 	@Test(priority = 1)
 	public void Login() throws Exception
 	{
-//		driver.get("https://allworkjob.com/data/copy-paste-day1-data1/");
-//		driver.manage().window().maximize();
-//		char Parent_Data= '2';
-//		List <WebElement> L1 = driver.findElementsByXPath("//a[contains(text(),'DAY')]");
-//		for(int i = 1; i<=L1.size(); i++)
-//		{
-//			String Child_DAY = driver.findElement(By.xpath("//a[.=\"DAY "+(+i)+"\"]")).getText(); 
-//			if(Child_DAY.equalsIgnoreCase("DAY 4"))
-//			{
-//				System.out.println("Day = "+Child_DAY);
-//				WebElement Choose_DAY = driver.findElement(By.xpath("//a[.='"+(Child_DAY)+"']"));
-//				((JavascriptExecutor)driver).executeScript("arguments[0].click();", Choose_DAY);
-//				WebElement Choose_DAY_DATE = driver.findElement(By.xpath("//a[.='Day"+(+i)+" Copy data "+(Parent_Data)+"'] | //a[.='day"+(+i)+" copy data "+(Parent_Data)+"']"));
-//				Choose_DAY_DATE.click();
-//				break;
-//			}
-//		}
+		driver.get("https://allworkjob.com/data/copy-paste-day1-data1/");
+		driver.manage().window().maximize();
+		char Parent_Data= '2';
+		List <WebElement> L1 = driver.findElementsByXPath("//a[contains(text(),'DAY')]");
+		for(int i = 1; i<=L1.size(); i++)
+		{
+			String Child_DAY = driver.findElement(By.xpath("//a[.=\"DAY "+(+i)+"\"]")).getText(); 
+			if(Child_DAY.equalsIgnoreCase("DAY 4"))
+			{
+				System.out.println("Day = "+Child_DAY);
+				WebElement Choose_DAY = driver.findElement(By.xpath("//a[.='"+(Child_DAY)+"']"));
+				((JavascriptExecutor)driver).executeScript("arguments[0].click();", Choose_DAY);
+				WebElement Choose_DAY_DATE = driver.findElement(By.xpath("//a[.='Day"+(+i)+" Copy data "+(Parent_Data)+"'] | //a[.='day"+(+i)+" copy data "+(Parent_Data)+"']"));
+				Choose_DAY_DATE.click();
+				break;
+			}
+		}
 		int i = 7;
 		String Page_Title = "COPY PASTE";
 		String Parent_Day= "6";
